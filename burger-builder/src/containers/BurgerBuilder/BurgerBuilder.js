e8ec72717a2977a1d194cbb184dd793a4740bccd
divyasanthoshi <divya.santhoshi1@gmail.com>
2020-09-08 08:23:50 -0500

    second commit

---
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/burger";
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2,
+    },
+  };
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
